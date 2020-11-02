95ea26a0072747d27f3affd29ce6646b7548aa89
marja katrina <38115150+murrha@users.noreply.github.com>
2020-11-01 18:53:16 -0800

    Delete ImmutableList.java

--- ImmutableList.java
+++
@@ -1,9 +0,0 @@
-public interface ImmutableList {
-    public boolean equals(final Object other);
-    public int length();
-    public int sum(); // empty list sum is defined as 0
-    public ImmutableList append(final ImmutableList other);
-    public boolean contains(final int value);
-    public String toString();
-    public int hashCode();
-} // ImmutableList
